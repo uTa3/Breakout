e6982f6e026f6a3d037dfd74a149a04f0cb12f77
Takahiro Ueno <uta20002020@gmail.com>
2020-10-18 00:40:02 +0900

    impl CD between ball and block

--- breakout/block.go
+++ breakout/block.go
@@ -3,15 +3,15 @@ package breakout
 import "github.com/hajimehoshi/ebiten/v2"
 
 type Block struct {
-	x, y  int
+	x, y          int
 	width, height int
-	isDead bool
-	image *ebiten.Image
+	isAlive       bool
+	image         *ebiten.Image
 }
 
 func (block *Block) Draw(screen *ebiten.Image) {
 	op := &ebiten.DrawImageOptions{}
-	op.GeoM.Translate(-float64(block.width), float64(block.height))
+	op.GeoM.Translate(-float64(block.width/2), float64(-block.height/2))
 	op.GeoM.Translate(float64(block.x), float64(block.y))
 	screen.DrawImage(block.image, op)
 }
--- breakout/game.go
+++ breakout/game.go
@@ -6,37 +6,35 @@ import (
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
-
 const (
 	ScreenWidth  = 480
 	ScreenHeight = 360
 )
 
 const (
-	initBarX = ScreenWidth/2
-	initBarY = 4*ScreenHeight/5
-	barWidth = 100
+	initBarX  = ScreenWidth / 2
+	initBarY  = 4 * ScreenHeight / 5
+	barWidth  = 100
 	barHeight = 20
 
-	initBallX = ScreenWidth/2
-	initBallY = 3*ScreenHeight/5
+	initBallX  = ScreenWidth / 2
+	initBallY  = 3 * ScreenHeight / 5
 	ballRadius = 4
 
-	blockWidth = 60
-	blockHeight = 10
-	row = 4
-	column = 5
+	blockWidth  = 60
+	blockHeight = 30
+	row         = 2
+	column      = 5
 )
 
 var (
-	barSpeed = 8
+	barSpeed     = 8
 	ballVelocity = 4
 )
 
-
-type Game struct{
-	bar *Bar
-	ball *Ball
+type Game struct {
+	bar    *Bar
+	ball   *Ball
 	blocks [][]*Block
 }
 
@@ -45,22 +43,22 @@ func NewGame() (*Game, error) {
 	var err error
 	// generate a new obj, bar, ball, block
 	g.bar = &Bar{
-		x: initBarX,
-		y: initBarY,
-		speed: barSpeed,
-		width: barWidth,
+		x:      initBarX,
+		y:      initBarY,
+		speed:  barSpeed,
+		width:  barWidth,
 		height: barHeight,
-		image: ebiten.NewImage(barWidth, barHeight),
+		image:  ebiten.NewImage(barWidth, barHeight),
 	}
 	g.bar.image.Fill(color.White)
 
 	g.ball = &Ball{
-		x: initBallX,
-		y: initBallY,
+		x:         initBallX,
+		y:         initBallY,
 		velocityX: ballVelocity,
 		velocityY: ballVelocity,
-		radius: ballRadius,
-		image: ebiten.NewImage(ballRadius, ballRadius),
+		radius:    ballRadius,
+		image:     ebiten.NewImage(ballRadius, ballRadius),
 	}
 	g.ball.image.Fill(color.White)
 
@@ -68,12 +66,12 @@ func NewGame() (*Game, error) {
 	for r := 0; r < row; r++ {
 		for c := 0; c < column; c++ {
 			g.blocks[r] = append(g.blocks[r], &Block{
-				x: 3*blockWidth/2 + 3*blockWidth/2*c,
-				y: blockHeight + 2*blockHeight*r,
-				width: blockWidth,
-				height: blockHeight,
-				isDead: false,
-				image: ebiten.NewImage(blockWidth, blockHeight),
+				x:       blockWidth + 3*blockWidth/2*c,
+				y:       blockHeight + 2*blockHeight*r,
+				width:   blockWidth,
+				height:  blockHeight,
+				isAlive: true,
+				image:   ebiten.NewImage(blockWidth, blockHeight),
 			})
 			g.blocks[r][c].image.Fill(color.White)
 		}
@@ -83,16 +81,80 @@ func NewGame() (*Game, error) {
 
 func (g *Game) Update() error {
 	g.bar.Update()
-	g.ball.Update(g.bar)
+	g.ball.Update()
+	// when the ball hits the bar
+	//  	 -------------
+	// here->|           |<- here
+	//  	 -------------
+	if g.bar.x-g.bar.width/2 < g.ball.x+g.ball.radius && // left
+		g.ball.x-g.ball.radius < g.bar.x+g.bar.width/2 && // right
+		g.bar.y-g.bar.height/2 < g.ball.y && g.ball.y < g.bar.y+g.bar.height/2 {
+		g.ball.velocityX = -g.ball.velocityX
+		// Avoid overlapping with the bar (more accurate)
+		if g.ball.x < g.bar.x {
+			g.ball.x = g.bar.x - g.bar.width/2 - g.ball.radius
+		} else if g.bar.x < g.ball.y {
+			g.ball.x = g.bar.x + g.bar.width/2 + g.ball.radius
+		}
+	}
+	//		  ↓  here  ↓
+	//  	 -------------
+	//       |           |
+	//  	 -------------
+	//		  ↑  here  ↑
+	if g.bar.y-g.bar.height/2 < g.ball.y+g.ball.radius && // upper
+		g.ball.y-g.ball.radius < g.bar.y+g.bar.height/2 && // lower
+		g.bar.x-g.bar.width/2 < g.ball.x && g.ball.x < g.bar.x+g.bar.width/2 {
+		g.ball.velocityY = -g.ball.velocityY
+		if g.ball.y < g.bar.y {
+			g.ball.y = g.bar.y - g.bar.height/2 - g.ball.radius
+		} else if g.bar.y < g.ball.y {
+			g.ball.y = g.bar.y + g.bar.height/2 + g.ball.radius
+		}
+	}
+
+	for r := 0; r < row; r++ {
+		for c := 0; c < column; c++ {
+			if g.blocks[r][c].x-g.blocks[r][c].width/2 < g.ball.x+g.ball.radius && // left
+				g.ball.x-g.ball.radius < g.blocks[r][c].x+g.blocks[r][c].width/2 && // right
+				g.blocks[r][c].y-g.blocks[r][c].height/2 < g.ball.y && g.ball.y < g.blocks[r][c].y+g.blocks[r][c].height/2 {
+				if g.blocks[r][c].isAlive {
+					g.ball.velocityX = -g.ball.velocityX
+					// Avoid overlapping with the bar (more accurate)
+					if g.ball.x < g.blocks[r][c].x {
+						g.ball.x = g.blocks[r][c].x - g.blocks[r][c].width/2 - g.ball.radius
+					} else if g.blocks[r][c].x < g.ball.y {
+						g.ball.x = g.blocks[r][c].x + g.blocks[r][c].width/2 + g.ball.radius
+					}
+				}
+				g.blocks[r][c].isAlive = false
+			}
+			if g.blocks[r][c].y-g.blocks[r][c].height/2 < g.ball.y+g.ball.radius && // upper
+				g.ball.y-g.ball.radius < g.blocks[r][c].y+g.blocks[r][c].height/2 && // lower
+				g.blocks[r][c].x-g.blocks[r][c].width/2 < g.ball.x && g.ball.x < g.blocks[r][c].x+g.blocks[r][c].width/2 {
+				if g.blocks[r][c].isAlive {
+					g.ball.velocityY = -g.ball.velocityY
+					if g.ball.y < g.blocks[r][c].y {
+						g.ball.y = g.blocks[r][c].y - g.blocks[r][c].height/2 - g.ball.radius
+					} else if g.blocks[r][c].y < g.ball.y {
+						g.ball.y = g.blocks[r][c].y + g.blocks[r][c].height/2 + g.ball.radius
+					}
+				}
+				g.blocks[r][c].isAlive = false
+			}
+		}
+	}
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	g.ball.Draw(screen)
 	g.bar.Draw(screen)
-	for _, rowBlock := range g.blocks {
-		for _, block := range rowBlock {
-			block.Draw(screen)
+	for r := 0; r < row; r++ {
+		for c := 0; c < column; c++ {
+			if g.blocks[r][c].isAlive {
+				g.blocks[r][c].Draw(screen)
+			}
 		}
 	}
 }
